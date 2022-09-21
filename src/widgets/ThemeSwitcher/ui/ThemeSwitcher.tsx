40a156220ddc234dafd3791ce0f80f87d02a54c7
kirill55580666 <kirill55580666@gmail.com>
2022-09-21 11:53:08 +0500

    stylelint + eslint i18n plugin

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,28 +1,25 @@
-import React from "react";
-import {Theme, useTheme} from "app/providers/ThemeProvider";
-import {classNames} from "shared/lib/classNames/classNames"
-import cls from './ThemeSwitcher.module.scss'
-import LightIcon from 'shared/assets/icons/theme-light.svg'
-import DarkIcon from 'shared/assets/icons/theme-dark.svg'
-import {Button, ThemeButton} from "shared/ui/Button/Button";
+import React from 'react';
+import { Theme, useTheme } from 'app/providers/ThemeProvider';
+import { classNames } from 'shared/lib/classNames/classNames';
+import LightIcon from 'shared/assets/icons/theme-light.svg';
+import DarkIcon from 'shared/assets/icons/theme-dark.svg';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
 
 interface ThemeSwitcherProps {
     className?: string;
 }
 
-export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
-    const {theme, toggleTheme} = useTheme();
+export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
+    const { theme, toggleTheme } = useTheme();
     return (
         <Button
-            className={classNames(cls.themeSwitcher, {}, [className])}
+            className={classNames('', {}, [className])}
             onClick={toggleTheme}
             theme={ThemeButton.CLEAR}
         >
-            {theme === Theme.DARK ?
-                <DarkIcon/>
-                :
-                <LightIcon/>
-            }
+            {theme === Theme.DARK
+                ? <DarkIcon />
+                : <LightIcon />}
         </Button>
     );
 };
