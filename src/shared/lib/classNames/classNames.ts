4eb884ead043f6110b4ce4b7719237ba300f2d09
kirill55580666 <kirill55580666@gmail.com>
2022-09-21 12:53:44 +0500

    setup jest + .eslintignore

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,6 +1,6 @@
 type Mods = Record<string, boolean | string>
 
-export function classNames(mainClass: string, mods?: Mods, additional?: string[]): string {
+export function classNames(mainClass: string, mods: Mods = {}, additional: string[] = []): string {
     return [
         mainClass,
         ...additional.filter(Boolean),
