567b012c59f587dfab3a8e24424cef53883d4dd8
kirill55580666 <kirill55580666@gmail.com>
2022-10-09 19:58:01 +0500

    return toggle sidebar for fix lint ts

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
 import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -16,18 +17,20 @@ export const Sidebar = ({ className }: SidebarProps) => {
         setCollapsed((prevState) => !prevState);
     };
 
+    const { t } = useTranslation();
+
     return (
         <div
             data-testid="sidebar"
             className={classNames(cls.sidebar, { [cls.collapsed]: collapsed }, [className])}
         >
-            {/* <Button */}
-            {/*    data-testid="sidebar-toggle" */}
-            {/*    type="button" */}
-            {/*    onClick={onToggle} */}
-            {/* > */}
-            {/*    toggle */}
-            {/* </Button> */}
+            <Button
+                data-testid="sidebar-toggle"
+                type="button"
+                onClick={onToggle}
+            >
+                {t('переключить')}
+            </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
