25d46314126a147aa452d46e8a78caa71c942c52
kirill55580666 <kirill55580666@gmail.com>
2022-10-20 17:30:55 +0500

    add AxiosDecorator.tsx for storybook

--- src/pages/ProfilePage/ui/ProfilePage.stories.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -3,6 +3,7 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
 import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+import AxiosDecorator from 'shared/config/storybook/AxiosDecorator/AxiosDecorator';
 import ProfilePage from './ProfilePage';
 
 export default {
@@ -17,9 +18,8 @@ const Template: ComponentStory<typeof ProfilePage> = (args) => <ProfilePage {...
 
 export const Normal = Template.bind({});
 Normal.args = {};
-Normal.decorators = [StoreDecorator({})];
+Normal.decorators = [StoreDecorator({}), AxiosDecorator];
 
 export const Dark = Template.bind({});
 Dark.args = {};
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
-Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
+Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({}), AxiosDecorator];
---
+++ src/shared/config/storybook/AxiosDecorator/AxiosDecorator.tsx
@@ -0,0 +1,46 @@
+import { useEffect } from 'react';
+import MockAdapter from 'axios-mock-adapter';
+import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
+import { ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { Story } from '@storybook/react';
+import { $api } from '../../../api/api';
+
+interface IProps {
+    children: any;
+    mock: (adapter: MockAdapter) => void;
+}
+
+const apiMock = new MockAdapter($api);
+
+const MockedComponent = ({ children, mock }: IProps) => {
+    useEffect(() => {
+        mock(apiMock);
+        return () => {
+            apiMock.reset();
+        };
+    });
+    return children;
+};
+
+export const AxiosDecorator = (StoryComponent: Story) => {
+    const mock = (apiMock: MockAdapter) => {
+        apiMock.onGet('/profile').reply(200, {
+            first: 'Кирилл',
+            lastname: 'Рыжиков',
+            age: 22,
+            currency: 'RUB',
+            country: 'Russia',
+            city: 'Ufa',
+            username: 'admin',
+            avatar: 'http://cyril-ryzhikov.ru/static/media/me.069b5d517d47e6360241.jpg',
+        });
+    };
+
+    return (
+        <MockedComponent mock={mock}>
+            <StoryComponent />
+        </MockedComponent>
+    );
+};
+
+export default AxiosDecorator;
